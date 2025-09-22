d5edd33f894028d820322fd71ad2c90c1288e6c6
rasha2023mahmoud <a01090178749z@gmail.com_GitHub>
2025-09-23 00:08:28 +0300

    update: add Title component and add Title component in page.js and add css Title style in global.js

---
+++ app/components/Title.js
@@ -0,0 +1,7 @@
+export default function Title({ text }) {
+  return (
+    <>
+      <h1 className="section-title">{text}</h1>
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import ShowBar from "./components/ShowBar";
 import Cataloge from "./components/Cataloge";
 import Offer from "./components/Offer";
+import Title from "./components/Title";
 export default function Home() {
   return (
     <main>
       <ShowBar />
       <Cataloge />
       <Offer />
+      <Title text="Trandy Products" />
     </main>
   );
 }
