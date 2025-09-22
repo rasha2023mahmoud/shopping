97179972cf238b0ef834c114337f3eceb32c8ad3
rasha2023mahmoud <a01090178749z@gmail.com_GitHub>
2025-09-22 21:22:02 +0300

    main

--- app/components/Offer.js
+++ app/components/Offer.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 export default function Offer() {
   return (
     <>
-      <div>
-        <div>
+      <div className="offer-container">
+        <div className="offer-card">
           <Image
             src="/images/offer-1.png"
             alt="offer-one"
             width={200}
             height={300}
+            className="offer-img"
           />
           <p>20% off the all order</p>
           <h2>20% off the all order</h2>
@@ -21,10 +22,11 @@ export default function Offer() {
             alt="offer-two"
             width={200}
             height={300}
+            className="offer-img"
           />
-          <p>20% off the all order</p>
-          <h2>20% off the all order</h2>
-          <button>Shop Now</button>
+          <p className="offer-text">20% off the all order</p>
+          <h2 className="offer-title">20% off the all order</h2>
+          <button className="offer-btn">Shop Now</button>
         </div>
       </div>
     </>
