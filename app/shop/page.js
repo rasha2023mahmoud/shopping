43e7b2a938d72fc571a683e8e556360240b82c20
rasha2023mahmoud <a01090178749z@gmail.com_GitHub>
2025-10-06 23:42:07 +0300

    update: shop/page.js

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,7 +1,55 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Link from "next/link";
+import FilterPrice from "./components/FilterPrice";
 export default function Shop() {
   return (
     <>
-      <div>Shop Page</div>
+      <Container fluid className="min-vh-100">
+        <Row>
+          <Row xs={1} md={2}>
+            <Col
+              className="first-section"
+              xs={12}
+              md={4}
+              lg={3}
+              style={{ flex: 1 }}
+            >
+              <div
+                className="shop-Title"
+                style={{
+                  width: "100%",
+                  height: "400px",
+                  backgroundColor: "#EDF1FF",
+                }}
+              >
+                <h1 style={{ fontWeight: "bold" }}> Our Shop</h1>
+                <Link href="/">
+                  <button className="home-btn">Home</button>
+                </Link>
+              </div>
+              {/* </Col>
+              <Col style={{ flex: 1 }}> */}
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+
+      <Container fluid className="min-vh-100">
+        <Row>
+          <Row xs={1} md={2}>
+            <Col
+              className="first-section"
+              md={2}
+              style={{ flex: 1, marginTop: "-282px;" }}
+            >
+              <FilterPrice />
+            </Col>
+            <Col md={10}></Col>
+          </Row>
+        </Row>
+      </Container>
     </>
   );
 }
