65e2219a998b1f0c17d9c5a094612c2fbc36b4ab
rasha2023mahmoud <a01090178749z@gmail.com GitHub>
2025-11-17 22:11:59 +0200

    add app/ContactUs/page.js

---
+++ app/ContactUs/page.js
@@ -0,0 +1,43 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Link from "next/link";
+import Footer from "../components/Footer";
+import Contacts from "./Contacts"
+export default function ShopCart() {
+  return (
+    <>
+      <Container fluid className="min-vh-90" style={{ marginBottom: "50px" }}>
+        <Row>
+          <Row xs={1} md={2}>
+            <Col
+              className="first-section"
+              xs={12}
+              md={4}
+              lg={3}
+              style={{ flex: 1 }}
+            >
+              <div
+                className="shop-Title"
+                style={{
+                  width: "100%",
+                  height: "400px",
+                  backgroundColor: "#EDF1FF",
+                }}
+              >
+                <h1 style={{ fontWeight: "bold" }}> Contact Us</h1>
+                <Link href="/">
+                  <button className="home-btn">Home</button>
+                </Link>
+              </div>
+              {/* </Col>
+              <Col style={{ flex: 1 }}> */}
+            </Col>
+          </Row>
+        </Row>
+            <Contacts/>
+        <Footer />
+      </Container>
+    </>
+  );
+}
